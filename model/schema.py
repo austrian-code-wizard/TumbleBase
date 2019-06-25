defbebabdc9cd9219fc27e20a49668b441dc0687
austrian-code-wizard <moritz.stephan@stanford.edu>
2019-06-25 18:25:03 +0200

    drafted schemas

--- model/schema.py
+++ model/schema.py
@@ -1,43 +1,44 @@
-from tumbleWeb.model.data_transfer_objects import Image, Message, Command
+from model.data_transfer_objects import Packet, Message
 from marshmallow import Schema, fields, post_load
 from datetime import datetime
 
 
-class ImageSchema(Schema):
-
+class PacketSchema(Schema):
+    # primary key and foreign keys
     id = fields.Int(dump_only=True)
 
-    image_path = fields.String(allow_none=False, required=True)
-    saved_at = fields.DateTime(dump_only=True)
+    # table attributes
+    packet_number = fields.Int(allow_none=False, required=True)
+    timestamp = fields.DateTime(dump_only=True)
+    content = fields.String(allow_none=False, required=True)
+
+    # relationships
+    message_id = fields.Int(allow_none=False, required=True)
 
     @post_load
     def init_model(self, data):
-        data["saved_at"] = datetime.utcnow()
-        return Image(**data)
-
-
-class CommandSchema(Schema):
-
-    id = fields.Int(dump_only=True)
-
-    command = fields.String(allow_none=False, required=True)
-    arguments = fields.String(allow_none=False, required=True)
-    saved_at = fields.DateTime(dump_only=True)
-
-    @post_load
-    def init_model(self, data):
-        data["saved_at"] = datetime.utcnow()
-        return Command(**data)
+        data["timestamp"] = datetime.utcnow()
+        return Packet(**data)
 
 
 class MessageSchema(Schema):
+    # primary key and foreign keys
+    id= fields.Int(dump_only=True)
 
-    id = fields.Int(dump_only=True)
+    # table attributes
+    message_number = fields.Int(allow_none=False, required=True)
+    address = fields.String(allow_none=False, required=True)
+    time_begin_sending = fields.DateTime(dump_only=True)
+    time_done_sending = fields.DateTime()
+    type = fields.String(allow_none=False, required=True)
+    value = fields.String(allow_none=False, required=True)
+    done = fields.Boolean(allow_none=False, required=True)
+    # message_type = # TODO: Implement with Enum
 
-    message = fields.String(allow_none=False, required=True)
-    saved_at = fields.DateTime(dump_only=True)
+    # relationships
+    # packets = fields.Nested(PacketSchema, dump_only=True) # TODO: Not sure if necessary
 
     @post_load
     def init_model(self, data):
-        data["saved_at"] = datetime.utcnow()
+        data["time_begin_sending"] = datetime.utcnow()
         return Message(**data)
